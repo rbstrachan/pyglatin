2ba66eb27c3fbd83e4890e3ad5baf66c79d0f1e2
Horace Grindelwald <rossbstrachan@gmail.com>
2018-02-21 22:09:21 +0000

    Made code readable

--- convert.py
+++ convert.py
@@ -1,14 +1,11 @@
-def rep():
-  pyg = "ay"
-  original = raw_input("Enter a word: ")
+suffix = "ay"
+original = raw_input("Enter a word: ")
   
-  if len(original) > 0 and original.isalpha():
-    word = original.lower()
-    first = word[0]
-    slice = word[1:len(word)]
-    final = slice + first + pyg
-    print final
-  else:
-    print "Im sorry. That didn't work. Please enter a word consisting of only alpha characters."
-
-rep()
+if len(original) > 0 and original.isalpha():
+  word = original.lower()
+  first_letter = word[0]
+  slice = word[1:len(word)]
+  final = slice + first_letter + suffix
+  print final
+else:
+  print "Im sorry. That didn't work. Please enter a word consisting of only alpha [non-numeric] characters."
