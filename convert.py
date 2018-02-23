ec1664881d3cd5601c5d3319a3512a8df8bcddbb
Horace Grindelwald <rossbstrachan@gmail.com>
2018-02-23 02:51:40 +0000

    Update convert.py
    Made compatible with Python3

--- convert.py
+++ convert.py
@@ -3,19 +3,19 @@ play_again = True
 suffix = "ay"
 
 while play_again:
-	original = raw_input("\nEnter a word to be converted into PygLatin: ")
+	original = input("\nEnter a word to be converted into PygLatin: ")
 	
 	if len(original) > 0 and original.isalpha():
 		word = original.lower()
 		first_letter = word[0]
 		slice = word[1:len(word)]
 		final = slice + first_letter + suffix
-		print "'" + original.title() + "'" + " in PygLatin is " + "'" + final.title() + "'"
-		print
+		print("'" + original.title() + "'" + " in PygLatin is " + "'" + final.title() + "'")
+		print()
 	else:
-		print "Im sorry. That didn't work. Please enter a word consisting of only alpha [non-numeric] characters."
-		print
+		print("Im sorry. That didn't work. Please enter a word consisting of only alpha [non-numeric] characters.")
+		print()
 	
-	again = raw_input("Would you like to convert another word? ")
+	again = input("Would you like to convert another word? ")
 	if again.lower() == "no" or again.lower() == "n":
 		play_again = False
